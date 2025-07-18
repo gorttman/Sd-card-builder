b5872326611977612ffdd914bd1e869a010db807
gorttman <brett@i3sec.com.au>
2025-07-18 07:06:21 +0000

    :sparkles: basic config read

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+
 set -euo pipefail
 
+# Where am I running
+cat /proc/mounts  | grep --quiet -i overlay 
+if [[ $? == 0 ]]; then 
+  ANSIBLE_BINARY_PATH="/usr/local/py-utils/bin"
+else
+  ANSIBLE_BINARY_PATH="/Library/Frameworks/Python.framework/Versions/3.13/bin"
+fi
+
+# Vars for use in both loading requirements and in running ansible-playbook
 REQS_FILE="./roles/requirements.yml"
 INVENTORY_FILE="variables/cli/hosts.ini"
 EXTRA_VARS_DIR="variables/cli"
-ANSIBLE_GALAXY="/usr/bin/ansible-galaxy"
-ANSIBLE_PLAYBOOK="/usr/bin/ansible-playbook"
+ANSIBLE_GALAXY="${ANSIBLE_BINARY_PATH}/ansible-galaxy"
+ANSIBLE_PLAYBOOK="${ANSIBLE_BINARY_PATH}/ansible-playbook"
 
 if [[ $# -lt 1 ]]; then
   echo "Usage: $0 <playbook.yml> [requirements.yml] [inventory.ini] [extra_vars.json]"
